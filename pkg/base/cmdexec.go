5f5827dd6faa35f369c25fed6156bd34bbe05630
Shmelev, Ruslan <Ruslan.Shmelev@emc.com>
2020-01-22 11:44:25 +0300

    [FABRIC-8501] use interface for osexecwrapper

--- pkg/base/cmdexec.go
+++ pkg/base/cmdexec.go
@@ -2,26 +2,44 @@ package base
 
 import (
 	"bytes"
+	"fmt"
 	"os/exec"
 	"strings"
 
 	"github.com/sirupsen/logrus"
 )
 
-// RunCmdFromStr gets command as a string, like: "netstat -n -a -p" and transform it into exec.Command type
-// and runs RunCmdFromCmdObj(cmd)
+type CmdExecutor interface {
+	RunCmd(cmd interface{}) (string, string, error)
+}
+
+type Executor struct {
+}
+
+func (e *Executor) RunCmd(cmd interface{}) (string, string, error) {
+	if cmdStr, ok := cmd.(string); ok {
+		return e.runCmdFromStr(cmdStr)
+	}
+	if cmdObj, ok := cmd.(*exec.Cmd); ok {
+		return e.runCmdFromCmdObj(cmdObj)
+	}
+	return "", "", fmt.Errorf("could not interpret command from %v", cmd)
+}
+
+// runCmdFromStr gets command as a string, like: "netstat -n -a -p" and transform it into exec.Command type
+// and runs runCmdFromCmdObj(cmd)
 // commands like: bash -c "something -param" are not supported
-func RunCmdFromStr(cmd string) (string, string, error) {
+func (e *Executor) runCmdFromStr(cmd string) (string, string, error) {
 	fields := strings.Fields(cmd)
 	name := fields[0]
 	if len(fields) > 1 {
-		return RunCmdFromCmdObj(exec.Command(name, fields[1:]...))
+		return e.runCmdFromCmdObj(exec.Command(name, fields[1:]...))
 	}
-	return RunCmdFromCmdObj(exec.Command(name))
+	return e.runCmdFromCmdObj(exec.Command(name))
 }
 
-// RunCmdFromCmdObj runs command and return stdout, stderr and error
-func RunCmdFromCmdObj(cmd *exec.Cmd) (outStr string, errStr string, err error) {
+// runCmdFromCmdObj runs command and return stdout, stderr and error
+func (e *Executor) runCmdFromCmdObj(cmd *exec.Cmd) (outStr string, errStr string, err error) {
 	ll := logrus.WithField("cmd", strings.Join(cmd.Args, " "))
 	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
--- pkg/base/cmdexec_test.go
+++ pkg/base/cmdexec_test.go
@@ -3,44 +3,49 @@ package base
 import (
 	"errors"
 	"fmt"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 type cmdAndResult struct {
-	cmdStr string
+	cmd    interface{}
 	strOut string
 	strErr string
 	err    error
 }
 
-func TestRunCmdFromStrWithoutErrors(t *testing.T) {
+func TestExecutorFromStrWithoutError(t *testing.T) {
 	var cmdPass = []cmdAndResult{
 		{"echo", "\n", "", nil},
 		{"echo 123", "123\n", "", nil},
 		{"echo 123 asd", "123 asd\n", "", nil},
+		{exec.Command("true"), "", "", nil},
 	}
 
+	e := Executor{}
+
 	for _, test := range cmdPass {
-		strOut, strErr, err := RunCmdFromStr(test.cmdStr)
-		assert.Nil(t, err, fmt.Sprintf("Unexpected error for cmd: \"%s\". Got error: %v", test.cmdStr, err))
-		assert.Equal(t, test.strOut, strOut, fmt.Sprintf("Unexpected stdout for cmd \"%s\".", test.cmdStr))
-		assert.Equal(t, test.strErr, strErr, fmt.Sprintf("Unexpected stderr for cmd \"%s\"", test.cmdStr))
+		strOut, strErr, err := e.RunCmd(test.cmd)
+		assert.Nil(t, err, fmt.Sprintf("Unexpected error for cmd: \"%s\". Got error: %v", test.cmd, err))
+		assert.Equal(t, test.strOut, strOut, fmt.Sprintf("Unexpected stdout for cmd \"%s\".", test.cmd))
+		assert.Equal(t, test.strErr, strErr, fmt.Sprintf("Unexpected stderr for cmd \"%s\"", test.cmd))
 	}
 }
 
-func TestRunCmdFromStrWitErrors(t *testing.T) {
-	cmdErr := cmdAndResult{
-		"false",
-		"",
-		"",
-		errors.New("exit status 1"),
+func TestExecutorFromStrAndExpectError(t *testing.T) {
+	var cmdErr = []cmdAndResult{
+		{"false", "", "", errors.New("exit status 1")},
+		{2, "", "", errors.New("could not interpret command from 2")},
 	}
 
-	strOut, strErr, err := RunCmdFromStr(cmdErr.cmdStr)
-	assert.Error(t, err)
-	assert.Contains(t, err.Error(), cmdErr.err.Error())
-	assert.Equal(t, strOut, cmdErr.strOut)
-	assert.Equal(t, strErr, cmdErr.strErr)
+	e := Executor{}
+
+	for _, test := range cmdErr {
+		strOut, strErr, err := e.RunCmd(test.cmd)
+		assert.Equal(t, test.strOut, strOut, fmt.Sprintf("Unexpected stdout for cmd \"%s\".", test.cmd))
+		assert.Equal(t, test.strErr, strErr, fmt.Sprintf("Unexpected stderr for cmd \"%s\"", test.cmd))
+		assert.Contains(t, err.Error(), test.err.Error())
+	}
 }
