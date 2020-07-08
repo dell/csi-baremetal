5ebee1c23d273013cf8498748e93655afc5ba669
Ruslan Shmelev <Ruslan.Shmelev@dell.com>
2020-07-08 10:59:38 +0300

    comment internal dependencies

--- variables.override.mk
+++ variables.override.mk
@@ -5,6 +5,8 @@ REGISTRY         := asdrepo.isus.emc.com:9042
 HEALTH_PROBE_BIN_URL := http://asdrepo.isus.emc.com:8081/artifactory/ecs-build/com/github/grpc-ecosystem/grpc-health-probe/0.3.1/grpc_health_probe-linux-amd64
 
 # go evn related
-GOPRIVATE_PART	 := GOPRIVATE=eos2git.cec.lab.emc.com/*
+GOPRIVATE_PART	 :=
+#GOPRIVATE=eos2git.cec.lab.emc.com/*
+
 # override variable in variables.mk
 GOPROXY_PART     := GOPROXY=http://asdrepo.isus.emc.com/artifactory/api/go/ecs-go-build,https://proxy.golang.org,direct
