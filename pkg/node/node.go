604267522ef8161dc608db9d87dc3fe19b21ba2f
Ruslan Shmelev <Ruslan.Shmelev@dell.com>
2020-07-15 11:53:31 +0300

    check context

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -86,6 +86,19 @@ func (s *CSINodeService) Probe(context.Context, *csi.ProbeRequest) (*csi.ProbeRe
 	}, nil
 }
 
+// checkRequestContext checks whether provided context is done or no, return error in case of done context
+func (s *CSINodeService) checkRequestContext(ctx context.Context, logger *logrus.Entry) error {
+	select {
+	case <-ctx.Done():
+		msg := fmt.Sprintf("context is done after volume lock. err: %s", ctx.Err())
+		logger.Warn(msg)
+		return errors.New(msg)
+	default:
+		logger.Info("Processing request")
+		return nil
+	}
+}
+
 // NodeStageVolume is the implementation of CSI Spec NodeStageVolume. Performs when the first pod consumes a volume.
 // This method mounts volume with appropriate VolumeID into the StagingTargetPath from request.
 // Receives golang context and CSI Spec NodeStageVolumeRequest
@@ -104,13 +117,9 @@ func (s *CSINodeService) NodeStageVolume(ctx context.Context, req *csi.NodeStage
 			ll.Warnf("Unlocking  volume with error %s", err)
 		}
 	}()
-	select {
-	case <-ctx.Done():
-		return nil, fmt.Errorf("context is done after volume lock. err: %s", ctx.Err())
-	default:
+	if err := s.checkRequestContext(ctx, ll); err != nil {
+		return nil, err
 	}
-
-	ll.Infof("Processing request: %v", req)
 
 	// Check arguments
 	if req.GetVolumeCapability() == nil {
@@ -189,13 +198,9 @@ func (s *CSINodeService) NodeUnstageVolume(ctx context.Context, req *csi.NodeUns
 			ll.Warnf("Unlocking  volume with error %s", err)
 		}
 	}()
-	select {
-	case <-ctx.Done():
-		return nil, fmt.Errorf("context is done after volume lock. err: %s", ctx.Err())
-	default:
+	if err := s.checkRequestContext(ctx, ll); err != nil {
+		return nil, err
 	}
-
-	ll.Infof("Processing request: %v", req)
 
 	// Check arguments
 	if len(req.GetVolumeId()) == 0 {
@@ -263,15 +268,9 @@ func (s *CSINodeService) NodePublishVolume(ctx context.Context, req *csi.NodePub
 			ll.Warnf("Unlocking  volume with error %s", err)
 		}
 	}()
-	select {
-	case <-ctx.Done():
-		return nil, fmt.Errorf("context is done after volume lock. err: %s", ctx.Err())
-	default:
+	if err := s.checkRequestContext(ctx, ll); err != nil {
+		return nil, err
 	}
-
-	ll.Infof("Processing request: %v", req)
-
-	// TODO: add lock for each volume
 
 	// Check arguments
 	if req.GetVolumeCapability() == nil {
@@ -474,12 +473,8 @@ func (s *CSINodeService) NodeUnpublishVolume(ctx context.Context, req *csi.NodeU
 			ll.Warnf("Unlocking volume with error %s", err)
 		}
 	}()
-	select {
-	case <-ctx.Done():
-		msg := fmt.Sprintf("context is done after volume lock. err: %s", ctx.Err())
-		ll.Warn(msg)
-		return nil, errors.New(msg)
-	default:
+	if err := s.checkRequestContext(ctx, ll); err != nil {
+		return nil, err
 	}
 
 	// Check arguments
