8197f141ee4bfcfa028fe1c7c1b996b5bc5c59c8
AnastasiaShemyakinskaya <anastasia.shemyakins@dell.com>
2020-08-28 10:27:26 +0000

    Add error handling

--- pkg/scheduler/openshift_patcher.sh
+++ pkg/scheduler/openshift_patcher.sh
@@ -8,8 +8,16 @@ RELEASE_NAME=scheduler-extender
 ARTIFACTORY=10.244.120.194:8081
 CSI_ARTIFACTORY_PATH=artifactory/atlantic-build/com/emc/atlantic/charts/csi
 
+checkErr() {
+	if [ $? -ne 0 ]; then
+		echo $1
+		exit 1
+	fi
+}
+
 helm install ${RELEASE_NAME} http://${ARTIFACTORY}/${CSI_ARTIFACTORY_PATH}/${IMAGE}/scheduler-extender-${IMAGE}.tgz \
 --set image.tag=${IMAGE} --set registry=${REGISTRY} --set port=${PORT}
+checkErr "Script failed: error during scheduler-extender installation."
 
 cat <<EOF >./policy.cfg
 {
@@ -34,9 +42,13 @@ if [ $exit_status -eq 0 ]; then
    # If ConfigMap contains predicates and priorities we will lost them in case of removing and replacing configmap.
    # But it hard to edit ConfigMap in bash script. Should we allow user to edit ConfigMap by its own.
    oc delete configmap ${POLICY_CONFIGMAP_NAME} -n openshift-config
+   checkErr "Script failed: error during execution \"os delete configmap\" command."
 fi
 oc create configmap -n openshift-config --from-file=policy.cfg ${POLICY_CONFIGMAP_NAME}
+checkErr "Script failed: error during execution \"oc create configmap\" command."
+
 oc patch Scheduler cluster --type='merge' -p '{"spec":{"policy":{"name":"'${POLICY_CONFIGMAP_NAME}'"}}}' --type=merge
+checkErr "Script failed: error during execution \"oc patch Scheduler cluster\" command."
 
 rm policy.cfg
 
