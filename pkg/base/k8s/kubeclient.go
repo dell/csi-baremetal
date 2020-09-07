79838a6a599beba456f78867b31d55a6ec1faa6b
yk <60463629+ykulazhenkov@users.noreply.github.com>
2020-09-07 16:53:30 +0300

    [ATLDEF-73] UpdateCRWithAttempts method fail fast if detects that object was modified (#54)

--- pkg/base/k8s/kubeclient.go
+++ pkg/base/k8s/kubeclient.go
@@ -248,7 +248,7 @@ func (k *KubeClient) ReadCRWithAttempts(name string, obj runtime.Object, attempt
 }
 
 // UpdateCRWithAttempts updates provided resource on k8s cluster with specified amount of attempts
-// Fails right away if resource is not found
+// Fails right away if resource is not found or was changed
 // Receives golang context and updated object that implements k8s runtime.Object interface
 // Returns error if something went wrong
 func (k *KubeClient) UpdateCRWithAttempts(ctx context.Context, obj runtime.Object, attempts int) error {
@@ -263,9 +263,12 @@ func (k *KubeClient) UpdateCRWithAttempts(ctx context.Context, obj runtime.Objec
 	defer ticker.Stop()
 
 	for i := 0; i < attempts; i++ {
-		if err = k.UpdateCR(ctxVal, obj); err == nil {
+		err = k.UpdateCR(ctxVal, obj)
+		if err == nil {
 			return nil
-		} else if k8sError.IsNotFound(err) {
+		}
+		// immediately return if object was removed or modified
+		if k8sError.IsNotFound(err) || k8sError.IsConflict(err) {
 			return err
 		}
 		ll.Warnf("Unable to update volume CR. Attempt %d out of %d with err %v", i, attempts, err)
