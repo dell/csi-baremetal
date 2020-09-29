7685f6d65ec84c3449c49c431d8b1a1d1c1d8177
Yury Kulazhenkov <yury.kulazhenkov@emc.com>
2020-09-29 17:27:23 +0300

    add copyright to files with code

--- pkg/scheduler/patcher/openshift_patcher.sh
+++ pkg/scheduler/patcher/openshift_patcher.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+#  Copyright © 2020 Dell Inc. or its subsidiaries. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 IMAGE="${IMAGE:-0.0.7}"
 REGISTRY="${REGISTRY:-10.244.120.194:9042}"
 PORT="${PORT:-8889}"
