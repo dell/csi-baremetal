d8d6230924ddf533be8805084ae9b7afd6510a46
Anastasia Shemyakinskaya <36507473+AnastasiaShemyakinskaya@users.noreply.github.com>
2020-10-14 15:12:54 +0300

    [Issue-117] Create csi-baremetal.nodes CRD (#121)
    Signed-off-by: AnastasiaShemyakinskaya <anastasia.shemyakins@dell.com>
    Signed-off-by: Mikhail Borisov <mikhail.borisov@emc.com>

---
+++ api/v1/csibmnodecrd/csibmnode_types.go
@@ -0,0 +1,53 @@
+/*
+Copyright © 2020 Dell Inc. or its subsidiaries. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package nodecrd
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	api "github.com/dell/csi-baremetal/api/generated/v1"
+)
+
+// +kubebuilder:object:root=true
+
+// CSIBMNode is the Schema for the CSIBMNode API
+type CSIBMNode struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec              api.CSIBMNode `json:"spec,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// CSIBMNodeList contains a list of CSIBMNode
+//+kubebuilder:object:generate=true
+type CSIBMNodeList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CSIBMNode `json:"items"`
+}
+
+func init() {
+	SchemeBuilderACR.Register(&CSIBMNode{}, &CSIBMNodeList{})
+}
+
+func (in *CSIBMNode) DeepCopyInto(out *CSIBMNode) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec = out.Spec
+}
