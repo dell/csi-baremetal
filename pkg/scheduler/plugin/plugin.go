d227e913dd7aa23ae649d7a8edc9561c32fad5ae
Mikhail Borisov <mikhail.borisov@emc.com>
2020-11-17 09:49:29 +0000

    panic calls removed

--- pkg/scheduler/plugin/plugin.go
+++ pkg/scheduler/plugin/plugin.go
@@ -17,6 +17,7 @@ limitations under the License.
 package plugin
 
 import (
+	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	framework "k8s.io/kubernetes/pkg/scheduler/framework/v1alpha1"
@@ -25,6 +26,7 @@ import (
 // CSISchedulerPlugin is a plugin that does placement decision based on information in AC CRD
 type CSISchedulerPlugin struct {
 	frameworkHandle framework.FrameworkHandle
+	logger *logrus.Entry
 }
 
 const (
@@ -52,26 +54,37 @@ func (c CSISchedulerPlugin) Name() string {
 
 // New initializes a new plugin and returns it.
 func New(configuration *runtime.Unknown, handle framework.FrameworkHandle) (framework.Plugin, error) {
-	sp := &CSISchedulerPlugin{frameworkHandle: handle}
+	sp := &CSISchedulerPlugin{
+		frameworkHandle: handle,
+		logger: logrus.New().WithField("component", Name),
+	}
+	sp.logger.Info("New scheduler instance created")
 	return sp, nil
 }
 
 // Filter filters out nodes which don't have ACs match to PVCs
 func (c CSISchedulerPlugin) Filter(pc *framework.PluginContext, pod *v1.Pod, nodeName string) *framework.Status {
-	panic("implement me")
+	c.logger.Info("Filter stage called")
+	return framework.NewStatus(framework.Success, "")
+	//panic("implement me")
 }
 
 // Score does balancing across the nodes for better performance. Nodes with more ACs should have highest scores
 func (c CSISchedulerPlugin) Score(pc *framework.PluginContext, p *v1.Pod, nodeName string) (int, *framework.Status) {
-	panic("implement me")
+	c.logger.Info("Score stage called")
+	return 0, framework.NewStatus(framework.Success, "")
+	//panic("implement me")
 }
 
 // Reserve does reservation of ACs
 func (c CSISchedulerPlugin) Reserve(pc *framework.PluginContext, p *v1.Pod, nodeName string) *framework.Status {
-	panic("implement me")
+	c.logger.Info("Reserve stage called")
+	return framework.NewStatus(framework.Success, "")
+	//panic("implement me")
 }
 
 // Unreserve un-reserver ACs
 func (c CSISchedulerPlugin) Unreserve(pc *framework.PluginContext, p *v1.Pod, nodeName string) {
-	panic("implement me")
+	c.logger.Info("Unreserve stage called")
+	//panic("implement me")
 }
