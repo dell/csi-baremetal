1201bc4a9b127063f76c9eb1951266abb63ed120
Anastasia Shemyakinskaya <36507473+AnastasiaShemyakinskaya@users.noreply.github.com>
2021-01-21 12:28:38 +0300

    [ISSUE-86] Handle owner addition/deletion during node publish/unpublish (#237)
    Signed-off-by: AnastasiaShemyakinskaya <anastasia.shemyakins@dell.com>

--- pkg/node/node_test.go
+++ pkg/node/node_test.go
@@ -91,7 +91,7 @@ var _ = Describe("CSINodeService NodePublish()", func() {
 			volumeCR := &vcrd.Volume{}
 			err = node.k8sClient.ReadCR(testCtx, testV1ID, volumeCR)
 			Expect(err).To(BeNil())
-			//Expect(volumeCR.Spec.Owners[0]).To(Equal(testPodName))
+			Expect(volumeCR.Spec.Owners[0]).To(Equal(testPodName))
 
 			// publish again such volume
 			resp, err = node.NodePublishVolume(testCtx, req)
@@ -102,7 +102,7 @@ var _ = Describe("CSINodeService NodePublish()", func() {
 			volumeCR = &vcrd.Volume{}
 			err = node.k8sClient.ReadCR(testCtx, testV1ID, volumeCR)
 			Expect(err).To(BeNil())
-			//Expect(len(volumeCR.Spec.Owners)).To(Equal(1))
+			Expect(len(volumeCR.Spec.Owners)).To(Equal(1))
 		})
 	})
 
@@ -342,6 +342,7 @@ var _ = Describe("CSINodeService NodeUnPublish()", func() {
 			err = node.k8sClient.ReadCR(testCtx, testV1ID, volumeCR)
 			Expect(err).To(BeNil())
 			Expect(volumeCR.Spec.CSIStatus).To(Equal(apiV1.VolumeReady))
+			Expect(volumeCR.Spec.Owners).To(BeNil())
 		})
 		//It("Should unpublish volume and don't change volume CR status", func() {
 		//	req := getNodeUnpublishRequest(testV1ID, targetPath)
@@ -421,7 +422,6 @@ var _ = Describe("CSINodeService NodeUnStage()", func() {
 			volumeCR := &vcrd.Volume{}
 			err = node.k8sClient.ReadCR(testCtx, testV1ID, volumeCR)
 			Expect(err).To(BeNil())
-			//Expect(volumeCR.Spec.Owners).To(BeNil())
 			Expect(volumeCR.Spec.CSIStatus).To(Equal(apiV1.Created))
 		})
 	})
@@ -467,7 +467,6 @@ var _ = Describe("CSINodeService NodeUnStage()", func() {
 			volumeCR := &vcrd.Volume{}
 			err = node.k8sClient.ReadCR(testCtx, testV1ID, volumeCR)
 			Expect(err).To(BeNil())
-			//Expect(volumeCR.Spec.Owners).To(BeNil())
 			Expect(volumeCR.Spec.CSIStatus).To(Equal(apiV1.Failed))
 		})
 
@@ -514,7 +513,6 @@ var _ = Describe("CSINodeService NodeUnStage()", func() {
 			volumeCR := &vcrd.Volume{}
 			err = node.k8sClient.ReadCR(testCtx, testV1ID, volumeCR)
 			Expect(err).To(BeNil())
-			//Expect(volumeCR.Spec.Owners).To(BeNil())
 			Expect(volumeCR.Spec.CSIStatus).To(Equal(apiV1.Created))
 		})
 	})
@@ -615,7 +613,7 @@ var _ = Describe("CSINodeService InlineVolumes", func() {
 			Expect(err).To(BeNil())
 
 			Expect(volumeCR.Spec.CSIStatus).To(Equal(apiV1.Published))
-			//Expect(volumeCR.Spec.Owners[0]).To(Equal(testPodName))
+			Expect(volumeCR.Spec.Owners[0]).To(Equal(testPodName))
 		})
 		It("Should fail to create inline volume in CreateVolume step", func() {
 			req := getNodePublishRequest(testV1ID, targetPath, *testVolumeCap)
