36687e89e851df47bb2bce500e1ee6eeca43cdc7
AnastasiaShemyakinskaya <anastasia.shemyakins@dell.com>
2021-01-25 13:57:51 +0000

    Add test
    Signed-off-by: AnastasiaShemyakinskaya <anastasia.shemyakins@dell.com>

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -1,3 +1,20 @@
+/*
+Copyright © 2020 Dell Inc. or its subsidiaries. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package metrics is for metrics, used in CSI
 package metrics
 
 import (
@@ -24,7 +41,7 @@ func NewVolumeMetrics() *VolumeMetrics {
 	vm.VolumeOperationsDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "volume_operations_duration",
 		Help:    "Volume operations methods duration",
-		Buckets: prometheus.ExponentialBuckets(0.001, 1.5, 25),
+		Buckets: prometheus.ExponentialBuckets(0.01, 1.5, 25),
 	}, []string{"method"})
 
 	return vm
@@ -56,7 +73,7 @@ func NewPartitionsMetrics() *PartitionsMetrics {
 	pm.PartitionOperationsDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "partition_operations_duration",
 		Help:    "Partition operations methods duration",
-		Buckets: prometheus.ExponentialBuckets(0.001, 1.5, 25),
+		Buckets: prometheus.ExponentialBuckets(0.5, 1.2, 20),
 	}, []string{"method"})
 
 	return pm
---
+++ pkg/metrics/metrics_test.go
@@ -0,0 +1,32 @@
+/*
+Copyright © 2020 Dell Inc. or its subsidiaries. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package metrics
+
+import (
+	"gotest.tools/assert"
+	"testing"
+)
+
+func TestMetrics(t *testing.T) {
+	vm := NewVolumeMetrics()
+	rVM := vm.Collect()
+	assert.Equal(t, vm.VolumeOperationsDuration, rVM)
+
+	pm := NewPartitionsMetrics()
+	rPM := pm.Collect()
+	assert.Equal(t, pm.PartitionOperationsDuration, rPM)
+}
