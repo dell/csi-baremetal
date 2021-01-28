8ebd980cb6fddeb2d8b8cbc986adeaf42706f807
Mikhail Borisov <38663549+mishoyama@users.noreply.github.com>
2021-01-28 13:25:15 +0300

    [ISSUE-243] put and remove os labels (#244)
    Signed-off-by: Mikhail Borisov <mikhail.borisov@emc.com>

--- pkg/crcontrollers/csibmnode/common/const.go
+++ pkg/crcontrollers/csibmnode/common/const.go
@@ -3,6 +3,11 @@
 package common
 
 const (
+	nodeKey = "csibmnodes.csi-baremetal.dell.com"
 	// NodeIDAnnotationKey hold key for annotation for node object
-	NodeIDAnnotationKey = "csibmnodes.csi-baremetal.dell.com/uuid"
+	NodeIDAnnotationKey = nodeKey + "/uuid"
+	// NodeOSNameLabelKey used as a label key for k8s node object to sort nodes by OS name (for example, Ubuntu)
+	NodeOSNameLabelKey = nodeKey + "/os-name"
+	// NodeOSVersionLabelKey used as a label key for k8s node object to sort nodes by OS version (for example, 19.04)
+	NodeOSVersionLabelKey = nodeKey + "/os-version"
 )
