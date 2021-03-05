c3f08d4ff62ddc618f23029160d656a413ce3434
Mikhail Borisov <mikhail.borisov@emc.com>
2021-03-05 11:59:14 +0000

    [ISSUE-308] fix TestNewCSIBMController test
    Signed-off-by: Mikhail Borisov <mikhail.borisov@emc.com>

--- pkg/crcontrollers/operator/controller_test.go
+++ pkg/crcontrollers/operator/controller_test.go
@@ -64,7 +64,7 @@ var (
 
 	osName    = "ubuntu"
 	osVersion = "18.04"
-	kernelVersion = "4.15.0"
+	kernelVersion = "4.15"
 	testNode1 = coreV1.Node{
 		ObjectMeta: metaV1.ObjectMeta{
 			Name:        "node-1",
@@ -427,7 +427,7 @@ func Test_checkAnnotationAndLabels(t *testing.T) {
 			currentOsVersionLabelValue: osVersion,
 			targetOsVersionLabelValue:  "19.10",
 			currentKernelVersionLabelValue: kernelVersion,
-			targetKernelVersionLabelValue: "5.4.0",
+			targetKernelVersionLabelValue: "5.4",
 		},
 	}
 
