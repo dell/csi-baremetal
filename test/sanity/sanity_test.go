ea8be1253468e1c39e56914e854cc6e4c122ccba
safronovD <danil_safronov@dell.com>
2021-03-18 17:08:27 +0300

    Use mock WrapLsblk
    Signed-off-by: safronovD <danil_safronov@dell.com>

--- test/sanity/sanity_test.go
+++ test/sanity/sanity_test.go
@@ -39,6 +39,7 @@ import (
 	"github.com/dell/csi-baremetal/pkg/base/rpc"
 	"github.com/dell/csi-baremetal/pkg/controller"
 	"github.com/dell/csi-baremetal/pkg/mocks"
+	mocklu "github.com/dell/csi-baremetal/pkg/mocks/linuxutils"
 	"github.com/dell/csi-baremetal/pkg/mocks/provisioners"
 	"github.com/dell/csi-baremetal/pkg/node"
 	p "github.com/dell/csi-baremetal/pkg/node/provisioners"
@@ -143,7 +144,7 @@ func newNodeSvc(kubeClient *k8s.KubeClient, nodeReady chan<- bool) {
 		for range time.Tick(10 * time.Second) {
 			err := csiNodeService.Discover()
 			if err != nil {
-				ll.Errorf("Discover failed: %v", err)
+				ll.Fatalf("Discover failed: %v", err)
 			}
 			doOnce.Do(func() {
 				nodeReady <- true
@@ -169,6 +170,9 @@ func prepareNodeMock(kubeClient *k8s.KubeClient, log *logrus.Logger) *node.CSINo
 		new(mocks.NoOpRecorder), featureconfig.NewFeatureConfig())
 
 	nodeService.VolumeManager = *node.NewVolumeManager(c, e, log, kubeClient, kubeClient, new(mocks.NoOpRecorder), nodeId)
+
+	listBlk := mocklu.GetMockWrapLsblk("/some/path")
+	nodeService.VolumeManager.SetListBlk(listBlk)
 
 	pMock := provisioners.GetMockProvisionerSuccess("/some/path")
 	nodeService.SetProvisioners(map[p.VolumeType]p.Provisioner{p.DriveBasedVolumeType: pMock})
