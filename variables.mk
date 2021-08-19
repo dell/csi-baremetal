d7425613ab27de364129d965e31a4d17e0582896
misanthropicat <48362383+misanthropicat@users.noreply.github.com>
2021-08-19 18:52:47 +0300

    [ISSUE-488] update version to 0.4.0
    Signed-off-by: misanthropicat <misanthr0picat@gmail.com>

--- variables.mk
+++ variables.mk
@@ -9,7 +9,7 @@ EXTENDER_CHART_PATH		:= charts/csi-baremetal-scheduler-extender
 
 ### version
 MAJOR            := 0
-MINOR            := 3
+MINOR            := 4
 PATCH            := 0
 PRODUCT_VERSION  ?= ${MAJOR}.${MINOR}.${PATCH}
 BUILD_REL_A      := $(shell git rev-list HEAD |wc -l)
