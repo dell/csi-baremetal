7020463b2e70b746030abbbf58a5e7801813a19d
safronovD <danil_safronov@dell.com>
2021-08-25 14:39:54 +0300

    add timeout
    Signed-off-by: safronovD <danil_safronov@dell.com>

--- pkg/crcontrollers/reservation/reservationcontroller.go
+++ pkg/crcontrollers/reservation/reservationcontroller.go
@@ -18,7 +18,8 @@ import (
 )
 
 const (
-	contextTimeoutSeconds = 60
+	contextTimeoutSeconds   = 60
+	reconcileTimeoutSeconds = 5
 )
 
 // Controller to reconcile aviliablecapacityreservation custom resource
@@ -95,7 +96,7 @@ func (c *Controller) handleReservationUpdate(ctx context.Context, log *logrus.En
 		placingPlan, err := capManager.PlanVolumesPlacing(ctx, volumes, requestedNodes)
 		if err == baseerr.ErrorRejectReservationRequest {
 			log.Warningf("Reservation request rejected due to another ACR in RESERVED state has request based on LVG")
-			return ctrl.Result{Requeue: true}, err
+			return ctrl.Result{RequeueAfter: reconcileTimeoutSeconds}, err
 		}
 		if err != nil {
 			log.Errorf("Failed to create placing plan: %s", err.Error())
