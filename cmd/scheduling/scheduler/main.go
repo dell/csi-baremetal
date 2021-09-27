46f1f4270e812b912fb07a3f5cb26781e10cedfc
Mikhail Borisov <mikhail.borisov@emc.com>
2021-09-27 09:05:22 +0000

    grpc downgraded
    Signed-off-by: Mikhail Borisov <mikhail.borisov@emc.com>

--- cmd/scheduling/scheduler/main.go
+++ cmd/scheduling/scheduler/main.go
@@ -17,22 +17,22 @@ limitations under the License.
 package main
 
 import (
-	/*"math/rand"
+	"math/rand"
 	"os"
 	"time"
 
 	"k8s.io/kubernetes/cmd/kube-scheduler/app"
 
-	"github.com/dell/csi-baremetal/pkg/scheduler/plugin"*/
+	"github.com/dell/csi-baremetal/pkg/scheduler/plugin"
 )
 
 func main() {
-	/*rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano())
 	// Register plugin to the scheduler framework.
 	command := app.NewSchedulerCommand(
 		app.WithPlugin(plugin.Name, plugin.New),
 	)
 	if err := command.Execute(); err != nil {
 		os.Exit(1)
-	}*/
+	}
 }
--- pkg/scheduler/plugin/plugin.go
+++ pkg/scheduler/plugin/plugin.go
@@ -17,14 +17,17 @@ limitations under the License.
 package plugin
 
 import (
-	//v1 "k8s.io/api/core/v1"
+	"context"
+
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	framework "k8s.io/kubernetes/pkg/scheduler/framework/v1alpha1"
+	schedulernodeinfo "k8s.io/kubernetes/pkg/scheduler/nodeinfo"
 )
 
 // CSISchedulerPlugin is a plugin that does placement decision based on information in AC CRD
 type CSISchedulerPlugin struct {
-	//frameworkHandle framework.FrameworkHandle
+	frameworkHandle framework.FrameworkHandle
 }
 
 const (
@@ -34,16 +37,16 @@ const (
 
 // please refer to https://kubernetes.io/docs/concepts/scheduling-eviction/scheduling-framework/ for details
 // Filter plugin
-//var _ framework.FilterPlugin = &CSISchedulerPlugin{}
-//
-//// Score plugin
-//var _ framework.ScorePlugin = &CSISchedulerPlugin{}
-//
-//// Reserve plugin
-//var _ framework.ReservePlugin = &CSISchedulerPlugin{}
-//
-//// Unreserve plugin
-//var _ framework.UnreservePlugin = &CSISchedulerPlugin{}
+var _ framework.FilterPlugin = &CSISchedulerPlugin{}
+
+// Score plugin
+var _ framework.ScorePlugin = &CSISchedulerPlugin{}
+
+// Reserve plugin
+var _ framework.ReservePlugin = &CSISchedulerPlugin{}
+
+// Unreserve plugin
+var _ framework.UnreservePlugin = &CSISchedulerPlugin{}
 
 // Name returns name of plugin
 func (c CSISchedulerPlugin) Name() string {
@@ -52,26 +55,31 @@ func (c CSISchedulerPlugin) Name() string {
 
 // New initializes a new plugin and returns it.
 func New(configuration *runtime.Unknown, handle framework.FrameworkHandle) (framework.Plugin, error) {
-	sp := &CSISchedulerPlugin{/*frameworkHandle: handle*/}
+	sp := &CSISchedulerPlugin{frameworkHandle: handle}
 	return sp, nil
 }
 
-//// Filter filters out nodes which don't have ACs match to PVCs
-//func (c CSISchedulerPlugin) Filter(pc *framework.PluginContext, pod *v1.Pod, nodeName string) *framework.Status {
-//	panic("implement me")
-//}
-//
-//// Score does balancing across the nodes for better performance. Nodes with more ACs should have highest scores
-//func (c CSISchedulerPlugin) Score(pc *framework.PluginContext, p *v1.Pod, nodeName string) (int, *framework.Status) {
-//	panic("implement me")
-//}
-//
-//// Reserve does reservation of ACs
-//func (c CSISchedulerPlugin) Reserve(pc *framework.PluginContext, p *v1.Pod, nodeName string) *framework.Status {
-//	panic("implement me")
-//}
-//
-//// Unreserve un-reserver ACs
-//func (c CSISchedulerPlugin) Unreserve(pc *framework.PluginContext, p *v1.Pod, nodeName string) {
-//	panic("implement me")
-//}
+// Filter filters out nodes which don't have ACs match to PVCs
+func (c CSISchedulerPlugin) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *schedulernodeinfo.NodeInfo) *framework.Status {
+	panic("implement me")
+}
+
+// Score does balancing across the nodes for better performance. Nodes with more ACs should have highest scores
+func (c CSISchedulerPlugin) Score(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) (int64, *framework.Status) {
+	panic("implement me")
+}
+
+// ScoreExtensions returns a ScoreExtensions interface if it implements one, or nil if does not.
+func (c CSISchedulerPlugin) ScoreExtensions() framework.ScoreExtensions {
+	panic("implement me")
+}
+
+// Reserve does reservation of ACs
+func (c CSISchedulerPlugin) Reserve(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) *framework.Status {
+	panic("implement me")
+}
+
+// Unreserve un-reserver ACs
+func (c CSISchedulerPlugin) Unreserve(ctx context.Context, state *framework.CycleState, p *v1.Pod, nodeName string) {
+	panic("implement me")
+}
