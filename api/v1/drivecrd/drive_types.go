959932c9b10353fb4dece3483ce602542b67855c
yurkov anton <yurkov-ant@yandex.ru>
2021-10-21 10:50:24 -0700

    add slot to message
    Signed-off-by: yurkov anton <yurkov-ant@yandex.ru>

--- api/v1/drivecrd/drive_types.go
+++ api/v1/drivecrd/drive_types.go
@@ -84,8 +84,8 @@ func (in *Drive) Equals(drive *api.Drive) bool {
 
 func (in *Drive) GetDriveDescription() string {
 	spec := in.Spec
-	description := fmt.Sprintf("Drive Details: SN='%s', Model='%s %s', Type='%s', Size='%d', NodeId='%s'",
-		spec.SerialNumber, spec.VID, spec.PID, spec.Type, spec.Size, spec.NodeId)
+	description := fmt.Sprintf("Drive Details: SN='%s', Model='%s %s', Slot='%s', Type='%s', Size='%d', NodeId='%s'",
+		spec.SerialNumber, spec.VID, spec.Slot, spec.PID, spec.Type, spec.Size, spec.NodeId)
 	// add firmware info only if detected
 	if spec.Firmware != "" {
 		description += fmt.Sprintf(", Firmware='%s'", spec.Firmware)
