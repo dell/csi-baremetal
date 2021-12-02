11ceb0fd346e756fda8cf80bb78e3d2a64b63034
AnnDutova <69694334+AnnDutova@users.noreply.github.com>
2021-12-02 12:28:31 +0300

    [ISSUE-626] E2e testing in GitHub Actions  (#584)
    Signed-off-by: AnnDutova <ann.dutova@list.ru>

--- variables.mk
+++ variables.mk
@@ -75,6 +75,9 @@ LDFLAGS := -ldflags "-X ${METRICS_PACKAGE}.Revision=${RELEASE_STR} -X ${METRICS_
 KIND_DIR := test/kind
 KIND     := ${KIND_DIR}/kind
 KIND_VER := 0.8.1
+KIND_CONFIG := kind.yaml
+KIND_IMAGE_VERSION := v1.18.2
+KIND_WAIT := 30s 
 
 ### ci vars
 # timeout for short test suite, must be parsable as Go time.Duration (60m, 2h)
