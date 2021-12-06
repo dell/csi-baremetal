6c48ae57b7d81d82effc8783de795cbfcc453f29
Alexander Mitsai <Alexander.Mitsai@dell.com>
2021-12-06 06:33:17 -0800

    Unit tests fixed

--- pkg/node/node_test.go
+++ pkg/node/node_test.go
@@ -406,10 +406,10 @@ var _ = Describe("CSINodeService NodeUnStage()", func() {
 
 	Context("NodeUnStage() success", func() {
 		It("Should unstage volume", func() {
-			Skip("NodeUnstageVolume skipped")
 			req := getNodeUnstageRequest(testV1ID, stagePath)
-			fsOps.On("UnmountWithCheck",
-				path.Join(req.GetStagingTargetPath(), stagingFileName)).Return(nil)
+			targetPath := path.Join(req.GetStagingTargetPath(), stagingFileName)
+			fsOps.On("UnmountWithCheck", targetPath).Return(nil)
+			fsOps.On("RmDir", targetPath).Return(nil)
 
 			resp, err := node.NodeUnstageVolume(testCtx, req)
 			Expect(resp).NotTo(BeNil())
@@ -484,12 +484,13 @@ var _ = Describe("CSINodeService NodeUnStage()", func() {
 
 	Context("NodeUnStage() concurrent requests", func() {
 		It("Should unstage volume one time", func() {
-			Skip("muted NodeUnstageVolume skipped")
 			req := getNodeUnstageRequest(testV1ID, stagePath)
 			secondUnstageErr := make(chan error)
 			// UnmountWithCheck should only once respond with no error
+			targetPath := path.Join(req.GetStagingTargetPath(), stagingFileName)
+			fsOps.On("RmDir", targetPath).Return(nil)
 			fsOps.On("UnmountWithCheck",
-				path.Join(req.GetStagingTargetPath(), stagingFileName)).Return(nil).Run(func(_ mock.Arguments) {
+				targetPath).Return(nil).Run(func(_ mock.Arguments) {
 				go func() {
 					_, err := node.NodeUnstageVolume(testCtx, req)
 					secondUnstageErr <- err
