e253d73ece4c7e2d58e5bfb2b5ae84be11227784
safronovD <danil_safronov@dell.com>
2022-04-29 17:31:11 +0300

    format
    Signed-off-by: safronovD <danil_safronov@dell.com>

--- variables.mk
+++ variables.mk
@@ -81,7 +81,7 @@ LDFLAGS         := -ldflags "-X ${METRICS_PACKAGE}.Revision=${RELEASE_STR} -X ${
 
 ### Kind
 KIND_BUILD_DIR     := ${PWD}/devkit/kind
-KIND_CONFIG_DIR	   := tests/kind
+KIND_CONFIG_DIR    := tests/kind
 KIND               := ${KIND_BUILD_DIR}/kind
 KIND_CONFIG        := kind.yaml
 KIND_IMAGE_VERSION := v1.19.11
