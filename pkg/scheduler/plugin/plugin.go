b81b01e091e000fc20ee61e61b67715e7790ded4
Shi, Crane <crane.shi@emc.com>
2022-10-31 16:27:46 +0800

    make csischedulerplugin unblock the normal scheduling in filter stage
    Signed-off-by: Shi, Crane <crane.shi@emc.com>

--- pkg/scheduler/plugin/plugin.go
+++ pkg/scheduler/plugin/plugin.go
@@ -18,6 +18,7 @@ package plugin
 
 import (
 	"context"
+	"k8s.io/klog/v2"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -60,7 +61,8 @@ func New(configuration runtime.Object, handle framework.Handle) (framework.Plugi
 
 // Filter filters out nodes which don't have ACs match to PVCs
 func (c CSISchedulerPlugin) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
-	panic("implement me")
+	klog.V(2).Infof("CSISchedulerPlugin Filer")
+	return nil
 }
 
 // Score does balancing across the nodes for better performance. Nodes with more ACs should have highest scores
