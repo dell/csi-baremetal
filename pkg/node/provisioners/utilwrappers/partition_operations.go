75706a20204f60e1c817dce91c06bf5b074ea198
libzhang <zlb0485@gmail.com>
2022-12-02 14:40:30 +0800

    remove mount check when doing partition sync check. (#929)
    Signed-off-by: Libin Zhang <Libin_Z@dell.com>
    
    Signed-off-by: Libin Zhang <Libin_Z@dell.com>
    Co-authored-by: Libin Zhang <Libin_Z@dell.com>

--- pkg/node/provisioners/utilwrappers/partition_operations.go
+++ pkg/node/provisioners/utilwrappers/partition_operations.go
@@ -180,11 +180,9 @@ func (d *PartitionOperationsImpl) SearchPartName(device, partUUID string) (strin
 		// sync partition table
 		_, errStr, err := d.SyncPartitionTable(device)
 		if err != nil {
-			isMounted, _ := d.IsMounted(strings.Trim(device, "/dev"))
 			if _, ok := err.(*exec.ExitError); ok &&
-				strings.Contains(errStr, "Device or resource busy") &&
-				isMounted {
-				ll.Errorf("Unable to sync partition table for device %s: %v due to device is mounted already", device, err)
+				strings.Contains(errStr, "Device or resource busy") {
+				ll.Warningf("Unable to sync partition table for device %s: %v due to device is busy", device, err)
 			} else {
 				// log and ignore error
 				ll.Errorf("Unable to sync partition table for device %s: %v", device, err)
