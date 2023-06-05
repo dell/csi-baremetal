9d10619b9b18017860fbca7bc632508e0f9ab144
Shi, Crane <crane.shi@emc.com>
2023-06-05 15:30:27 +0800

    now also include drive del event for storagegroup controller
    Signed-off-by: Shi, Crane <crane.shi@emc.com>

--- pkg/crcontrollers/storagegroup/storagegroupcontroller.go
+++ pkg/crcontrollers/storagegroup/storagegroupcontroller.go
@@ -61,10 +61,6 @@ func (c *Controller) SetupWithManager(mgr ctrl.Manager) error {
 		WithOptions(controller.Options{}).
 		Watches(&source.Kind{Type: &drivecrd.Drive{}}, &handler.EnqueueRequestForObject{}).
 		WithEventFilter(predicate.Funcs{
-			DeleteFunc: func(e event.DeleteEvent) bool {
-				_, ok := e.Object.(*sgcrd.StorageGroup)
-				return ok
-			},
 			UpdateFunc: func(e event.UpdateEvent) bool {
 				return c.filterUpdateEvent(e.ObjectOld, e.ObjectNew)
 			},
